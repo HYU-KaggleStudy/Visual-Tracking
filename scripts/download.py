f39e0d11c251fe3d87f3910b38b43f318b78905f
MaybeS <maybe@hanyang.ac.kr>
2018-01-05 00:53:23 +0900

    Update download script

--- scripts/download.py
+++ scripts/download.py
@@ -4,11 +4,11 @@ from zipfile import ZipFile
 from os import remove
 
 targets = ["http://cvlab.hanyang.ac.kr/tracker_benchmark/seq/Dog.zip",
-"http://cvlab.hanyang.ac.kr/tracker_benchmark/seq/Car2.zip"
-]
+"http://cvlab.hanyang.ac.kr/tracker_benchmark/seq/Car2.zip"]
 
-
+objs = []
 for tar in targets:
+    objs.append(Path(tar).stem)
     path = Path("./train/") / Path(tar).name
     print ('Download', str(path))
     request.urlretrieve(tar, str(path))
@@ -18,4 +18,7 @@ for tar in targets:
     zip.close()
     remove(str(path))
     print (str(path), 'done!')
+
+with open('labels.txt', 'w') as f:
+    f.write('\n'.join(objs))
 
